fb21c5006f355fce34ab7fb8f7eef3de725996e5
Scott <cvasiuc.dima@gmail.com>
2023-08-02 19:23:32 +0300

    completion of the first version

---
+++ src/index.ts
@@ -0,0 +1,2 @@
+import './styles.scss';
+document.body.innerHTML = '12321';
---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+const devServer = isDev => !isDev ? {} : {
+  devServer: {
+    open: {
+      app: {
+        name: 'firefox',
+      },
+    },
+    hot: true,
+    port: 3000,
+    // contentBase: path.join(__dirname, 'public'),
+  }
+};
+
+const esLintPlugin = (isDev) => isDev ? [] : [new ESLintPlugin({ extensions: ['ts', 'js'] })];
+
+module.exports = ({ development }) => ({
+  mode: development ? 'development' : 'production',
+  devtool: development ? 'inline-source-map' : false,
+  entry: {
+    main: './src/index.ts',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    assetModuleFilename: 'assets/[hash][ext]',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[tj]s$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  plugins: [
+    ...esLintPlugin(development),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    // new CopyPlugin({
+    // 	patterns: [
+    // 		{ from: './folderName', to: './folderName' }
+    // 	],
+    // }),
+  ],
+  ...devServer(development),
+});
